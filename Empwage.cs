7605e79e21d84d8c00143787d8e654f5d52d62a5
Aneesh <katta.aneesh597@gmail.com>
2022-11-10 18:01:21 +0530

    added uc4-empwage

--- Empwage.cs
+++ Empwage.cs
@@ -11,26 +11,26 @@ namespace EmpWage_Batch222
         static void Main()
         {
             Console.WriteLine("Welcome to Employee Wage Computation problem");
-            int is_fulltime = 1;
-            int is_parttime = 2;
+            const int is_fulltime = 1;
+            const int is_parttime = 2;
             int emphrs = 0;
             int rateperhr = 20;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-            if (empCheck == is_fulltime)
-            {
+            switch (empCheck)
+            { 
+                case is_fulltime:
                 Console.WriteLine("Employee is present full time");
                 emphrs = 8;
-            }
-            else if(empCheck == is_parttime)
-            {
+                break;
+                case is_parttime:
                 Console.WriteLine("Employee is present part time");
                 emphrs = 4;
-            }
-            else
-            {
+                break;
+                default:
                 Console.WriteLine("Employee is absent");
                 emphrs = 0;
+                break;
             }
             int DailyWage = emphrs * rateperhr;
             Console.WriteLine("Daily wage of employee is " + DailyWage);
