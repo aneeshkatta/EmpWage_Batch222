5e4270ef4449c499fe9fbf0f13ddb56c73f78837
Aneesh <katta.aneesh597@gmail.com>
2022-11-10 14:54:16 +0530

    added welcome message

---
+++ Empwage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage_Batch222
+{
+     class Empwage
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation problem");
+        }
+    }
+}
