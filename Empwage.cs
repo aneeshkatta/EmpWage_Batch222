442d8f3aec3b7b0c661df64d39d4087194d7afc3
Aneesh <katta.aneesh597@gmail.com>
2022-11-10 18:15:10 +0530

    uc5-monthlywage

--- Empwage.cs
+++ Empwage.cs
@@ -13,27 +13,37 @@ namespace EmpWage_Batch222
             Console.WriteLine("Welcome to Employee Wage Computation problem");
             const int is_fulltime = 1;
             const int is_parttime = 2;
-            int emphrs = 0;
+            int emphrs = 0, dailywage = 0;
+            int totalemphrs = 0;
             int rateperhr = 20;
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            switch (empCheck)
-            { 
-                case is_fulltime:
-                Console.WriteLine("Employee is present full time");
-                emphrs = 8;
-                break;
-                case is_parttime:
-                Console.WriteLine("Employee is present part time");
-                emphrs = 4;
-                break;
-                default:
-                Console.WriteLine("Employee is absent");
-                emphrs = 0;
-                break;
+            const int No_OF_Working_days = 20;
+            int monthlywage = 0;
+            
+            for (int day = 1; day <= No_OF_Working_days; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                switch (empCheck)
+                {
+                    case is_fulltime:
+                        Console.WriteLine("Employee is present full time");
+                        emphrs = 8;
+                        break;
+                    case is_parttime:
+                        Console.WriteLine("Employee is present part time");
+                        emphrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        emphrs = 0;
+                        break;
+                }
+                dailywage = emphrs * rateperhr;
+                Console.WriteLine("day{0}-Dailywage is {1}", day, dailywage);
+                monthlywage += dailywage;
             }
-            int DailyWage = emphrs * rateperhr;
-            Console.WriteLine("Daily wage of employee is " + DailyWage);
+           
+            Console.WriteLine("Monthly wage of employee is " + monthlywage);
         }
     }
 }
